18b970ca08e70383fc6a13894c361d588f8a5e7b
Kushan Joshi <kushan.joshi@mapbox.com>
2018-07-24 16:13:51 +0530

    Update stickler.config.js

--- stickler.config.js
+++ stickler.config.js
@@ -1,21 +1,8 @@
 'use strict';
-// _underreact-site
-// examples
 
 module.exports = {
   ignore: ['_underreact-site', 'examples'],
   jsLint: {
-    node: true,
-    overrides: [
-      {
-        files: ['forward-event-client/**'],
-        es5: true,
-        browser: true
-      },
-      {
-        files: ['test/**'],
-        jest: true
-      }
-    ]
+    node: true
   }
 };
