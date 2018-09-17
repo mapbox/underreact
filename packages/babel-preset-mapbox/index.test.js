af84af013bc1743f5c00eee7740ac3486a328a69
Kushan Joshi <0o3ko0@gmail.com>
2018-09-17 19:55:15 +0530

    Update babel to v7

--- packages/babel-preset-mapbox/index.test.js
+++ packages/babel-preset-mapbox/index.test.js
@@ -1,57 +1,74 @@
 'use strict';
+const babel = require('@babel/core');
 
-const babel = require('babel-core');
+const { babelPresetMapbox: preset } = require('./index.js');
 
-const preset = require('./index.js');
-
-const transform = (code, options) =>
-  babel.transform(code, {
-    presets: [[require.resolve('./index.js'), options]],
-    // This is needed 'babel-plugin-transform-react-jsx-source' throws an error without a filename
+const transform = (code, options) => {
+  return babel.transformSync(code, {
+    presets: [[require('./index.js'), options]],
+    // This is needed by 'babel/plugin-react'
     filename: 'test'
   }).code;
+};
+
+afterEach(() => {
+  delete process.env.BABEL_ENV;
+  delete process.env.BROWSERSLIST;
+});
 
 test('Right order of configuration in production', () => {
   process.env.BABEL_ENV = 'production';
   expect(preset()).toMatchSnapshot();
-  delete process.env.BABEL_ENV;
 });
 
 test('Right order of configuration in development', () => {
   process.env.BABEL_ENV = 'development';
   expect(preset()).toMatchSnapshot();
-  delete process.env.BABEL_ENV;
 });
 
 test('Right order of configuration in test', () => {
   process.env.BABEL_ENV = 'test';
   expect(preset()).toMatchSnapshot();
-  delete process.env.BABEL_ENV;
 });
 
 test('Passes opts correctly', () => {
   const config = preset(null, {
-    'babel-preset-react': {
-      abc: 'def'
+    '@babel/preset-react': {
+      abc: 'def',
+      useBuiltIns: false
     },
     'non-existent': {
       a: 'a'
     },
-    'transform-class-properties': { xyz: '123' }
+    'babel-plugin-transform-dynamic-import': { xyz: '123' }
   });
+
   expect(config.presets).toContainEqual([
-    'babel-preset-react',
+    '@babel/preset-react',
     {
-      abc: 'def'
+      abc: 'def',
+      development: true,
+      useBuiltIns: false
     }
   ]);
+
   expect(config.plugins).toContainEqual([
-    'babel-plugin-transform-class-properties',
+    'babel-plugin-transform-dynamic-import',
     {
       xyz: '123'
     }
   ]);
-  expect(config).toMatchSnapshot();
+});
+
+test('Does not mess with a plugin which is not to be loaded', () => {
+  process.env.BABEL_ENV = 'development';
+  const config = preset(null, {
+    'babel-plugin-transform-dynamic-import': { xyz: '123' }
+  });
+
+  expect(
+    config.plugins.find(p => p[0] === 'babel-plugin-transform-dynamic-import')
+  ).toBeUndefined();
 });
 
 test('BROWSERSLIST env var works in non test env ', () => {
@@ -61,48 +78,23 @@ test('BROWSERSLIST env var works in non test env ', () => {
   );
   const config = preset(null);
   expect(config.presets).toMatchSnapshot();
-  delete process.env.BABEL_ENV;
-  delete process.env.BROWSERSLIST;
 });
 
-test('Throws error when both BROWSERSLIST and babel-preset-env are provided ', () => {
+test('Throws error when both BROWSERSLIST and babel/preset-env are provided ', () => {
   process.env.BROWSERSLIST = 'something';
   expect(() =>
     preset(null, {
-      'babel-preset-env': {}
+      '@babel/preset-env': {}
     })
   ).toThrowError();
-  delete process.env.BROWSERSLIST;
 });
 
-test("BROWSERSLIST env var doesn't get injected in test env ", () => {
-  process.env.BROWSERSLIST = ['>0.25%', 'not ie 11', 'not op_mini all'].join(
-    ','
-  );
-  const config = preset(null);
-  expect(config.presets).toMatchSnapshot();
-  delete process.env.BROWSERSLIST;
-});
-
-test('Works with React', () => {
+test('BROWSERSLIST works when importing babel polyfill ', () => {
+  process.env.BABEL_ENV = 'development';
+  process.env.BROWSERSLIST = ['chrome 57'].join(',');
   expect(
     transform(`
-        import React from 'react';
-        const F = () => <Zoo/>;
+        import '@babel/polyfill'
     `)
-  ).toMatchSnapshot();
-});
-
-test('Throws error when incorrect options are provided', () => {
-  expect(() => transform(``, 45)).toThrowErrorMatchingSnapshot();
-});
-
-test('Applies options correctly to internal plugins and presets', () => {
-  expect(
-    transform(`const F = () => <Zoo/>;`, {
-      'transform-react-jsx': {
-        pragma: 'dom'
-      }
-    })
   ).toMatchSnapshot();
 });
